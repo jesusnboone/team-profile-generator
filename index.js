26233dc9a2e37f29f1c818120e22ed653463c7fd
jesusnboone <jesusnboone@gmail.com>
2020-07-25 13:45:33 -0500

    added if function for manager position

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const inquirer = require('inquirer');
 const Employee = require('./lib/Employee')
+const Manager = require('./lib/Manager')
 
 function Team() {
-    return inquirer
+    inquirer
     .prompt([
         {
             type: 'text',
@@ -18,18 +19,54 @@ function Team() {
             type: 'text',
             name: 'email',
             message: 'Please enter their email address.'
+        },
+        {
+            type: 'list',
+            name: 'position',
+            message: 'What is the work position of this employee?',
+            choices: ['Manager', 'Engineer', 'Intern'],
+
         }
 
-    
-    
     ])
-    .then(({ name, id, email }) => {
+        .then(({ name, id, email, position }) => {
+            if (position === 'Manager') {
+                this.employee = new Manager(name);
+                this.employee.id = id;
+                this.employee.email = email;
+                this.employee.role = position;
+                inquirer
+                .prompt({
+                    type: 'number',
+                    name: 'officeNumber',
+                    message: 'Please enter the office number of this manager.'
+                })
+                .then(({officeNumber}) => {
+                    this.employee.officeNumber = officeNumber
+            
+                    console.log(this.employee.getRole());}
+                )
+            }
+        })
+
+/*    .then(({ name, id, email, position, officeNumber}) => {
+        if (position === 'Manager') {
+        this.employee = new Manager(name);
+        this.employee.id = id;
+        this.employee.email = email;
+        this.employee.role = position;
+        this.employee.officeNumber = officeNumber
+
+        console.log(this.employee.getRole());}
+    })
+
+    /*.then(({ name, id, email }) => {
         this.employee = new Employee(name);
         this.employee.id = id;
         this.employee.email = email;
 
         console.log(this.employee.getRole());
-    })
+    })*/
 }
 
 Team();
